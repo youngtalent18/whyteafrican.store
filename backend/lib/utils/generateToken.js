804c6a701d7da5a6fe84ee5f691abdb9cefad564
Stephen Anti <stephenanti63@gmail.com>
2026-06-05 02:18:56 -0700

    1-modified file

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -23,7 +23,7 @@ export const setCookies = (
     {
       httpOnly: true,
       secure: isProduction,
-      sameSite: "none",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 15 * 60 * 1000,
     }
   );
@@ -34,7 +34,7 @@ export const setCookies = (
     {
       httpOnly: true,
       secure: isProduction,
-      sameSite: "none",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge:
         7 * 24 * 60 * 60 * 1000,
     }
